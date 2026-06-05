6db9c9e8c6fba12b5cc81e047c4f3d15284bf069
Maksym Martyn <maksroomm@gmail.com>
2026-06-05 23:42:59 +0300

    nodejs: REST API, JSON, express routing

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,26 @@
+/*
 const fs = require('fs');
 let data = "Hello from Node.js W"
 fs.writeFileSync("hello.txt",data)
 const text = fs.readFileSync('./hello.txt', 'utf8')
 console.log(text)
+ */
+const users = [
+    { id: 1, name: "Максим", age: 19 },
+    { id: 2, name: "Андрій", age: 25 },
+];
+
+const express = require("express");
+const app = express();
+
+app.get('/users', (req,res) => {
+    res.json(users);
+});
+
+app.get('/users/:id', (req,res) => {
+    const user = users.find(u => u.id === Number(req.params.id));
+    if(!user) return res.status(404).json({ message: 'Not found' });
+    res.json(user);
+})
+
+app.listen(3000);
